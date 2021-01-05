c2471c577514516a6de68926f4af429b7d4f350c
frimpongopoku <pongofrimi@gmail.com>
2021-01-05 06:31:06 +0400

    Initial Commit

---
+++ Shared/GenerateLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenerateLocationApp.swift
+//  Shared
+//
+//  Created by frimpongOpokuAgyemang on 05/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct GenerateLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
